b6587d17a3878efb820e27d3394ca41beb68cf1b
jsm-txt <hose153@gmail.com>
2020-11-24 02:07:26 -0600

    Pytest
    Ran Pytest

--- hero.py
+++ hero.py
@@ -2,7 +2,7 @@ import random
 from ability import Ability
 from armor import Armor
 from weapon import Weapon
-
+from team import Team
 
 class Hero:
     # We want our hero to have a default "starting_health",
@@ -127,51 +127,51 @@ class Hero:
 
 
 
-if __name__ == "__main__":
-    # If you run this file from the terminal
-    # this block is executed.
+# if __name__ == "__main__"
+#     # If you run this file from the terminal
+#     # this block is executed.
 
     
-    # print(my_hero.name)
-    # print(my_hero.current_health)
-    # my_hero.fight("slime")
+#     # print(my_hero.name)
+#     # print(my_hero.current_health)
+#     # my_hero.fight("slime")
 
     
-    # hero = Hero("Grace Hopper", 200)
+#     # hero = Hero("Grace Hopper", 200)
     
-    # print(hero.abilities)
+#     # print(hero.abilities)
 
-    # If you run this file from the terminal
-    # this block of code is executed.
-    # ability = Ability("Great Debugging", 50)
-    # another_ability = Ability("Smarty Pants", 90)
-    # hero = Hero("Grace Hopper", 200)
-    # hero.add_ability(ability)
-    # hero.add_ability(another_ability)
-    # print(hero.attack())
+#     # If you run this file from the terminal
+#     # this block of code is executed.
+#     # ability = Ability("Great Debugging", 50)
+#     # another_ability = Ability("Smarty Pants", 90)
+#     # hero = Hero("Grace Hopper", 200)
+#     # hero.add_ability(ability)
+#     # hero.add_ability(another_ability)
+#     # print(hero.attack())
 
-    # hero = Hero("Grace Hopper", 200)
-    # hero.take_damage(50)
+#     # hero = Hero("Grace Hopper", 200)
+#     # hero.take_damage(50)
 
-    # print(hero.current_health)
-    # hero.take_damage(150)
-    # print(hero.is_alive())
-    # hero.take_damage(15000)
-    # print(hero.is_alive())
-    hero = Hero("Grace Hopper", 200)
-    ability = Ability("Great Debugging", 50)
-    weapon = Weapon("Lasso of Truth", 90)
-    hero.add_ability(ability)
-    hero.add_weapon(weapon)
-    print(hero.attack())
-    shield = Armor("Shield", 50)
-    hero.add_armor(shield)
+#     # print(hero.current_health)
+#     # hero.take_damage(150)
+#     # print(hero.is_alive())
+#     # hero.take_damage(15000)
+#     # print(hero.is_alive())
+#     hero = Hero("Grace Hopper", 200)
+#     ability = Ability("Great Debugging", 50)
+#     weapon = Weapon("Lasso of Truth", 90)
+#     hero.add_ability(ability)
+#     hero.add_weapon(weapon)
+#     print(hero.attack())
+#     shield = Armor("Shield", 50)
+#     hero.add_armor(shield)
     
 
-    # hero2 = Hero("slime", 200)
-    # hero2.add_ability(ability)
-    # hero2.add_armor(shield)
+#     # hero2 = Hero("slime", 200)
+#     # hero2.add_ability(ability)
+#     # hero2.add_armor(shield)
 
-    # hero.fight(hero2)
+#     # hero.fight(hero2)
 
     
